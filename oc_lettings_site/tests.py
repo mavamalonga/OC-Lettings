4490167c8a542983b0c6515af6c2dfb5fe17783c
Alpha <mavamalonga.alpha@gmail.com>
2022-03-23 15:59:54 +0100

    init test

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,11 @@
-def test_dummy():
-    assert 1
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+    
+class URLTests(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Welcome to Holiday Homes', response.content)
+
+
+# py manage.py test
