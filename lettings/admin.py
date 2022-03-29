64b78ab87b65ee7cff984c157758c68344482d2d
Alpha <mavamalonga.alpha@gmail.com>
2022-03-29 15:41:11 +0200

    upgrade admin

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import Letting, Address
-from profiles.models import Profile
 
 
 class LettingAdmin(admin.ModelAdmin):
@@ -12,6 +10,3 @@ class AddressAdmin(admin.ModelAdmin):
 	pass
 admin.site.register(Address, AddressAdmin)
 
-class ProfileAdmin(admin.ModelAdmin):
-	pass
-admin.site.register(Profile, ProfileAdmin)
