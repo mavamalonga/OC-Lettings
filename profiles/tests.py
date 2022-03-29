05e7afe88e013191288d54afa1584a27bfa0606f
Alpha <mavamalonga.alpha@gmail.com>
2022-03-29 15:37:13 +0200

    profiles test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+import unittest
+from django.test import Client
 
-# Create your tests here.
+class ProfilesTest(unittest.TestCase):
+    def test_profiles(self):
+        client = Client()
+        response = client.get('/profiles/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>Profiles</h1>', response.content)
+
+    def test_profile(self):
+        client = Client()
+        response = client.get('/profiles/alpha/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<p>Email: alpha@exemple.com</p>', response.content)
