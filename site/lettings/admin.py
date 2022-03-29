32fa7b2a47892a3b9431cad7d64295ecccc206dc
Alpha <mavamalonga.alpha@gmail.com>
2022-03-29 16:51:08 +0200

    django-admin interface bug

--- site/lettings/admin.py
+++ site/lettings/admin.py
@@ -4,3 +4,7 @@ from .models import Letting, Address
 
 admin.site.register(Letting)
 admin.site.register(Address)
+
+# resolve interface bug sticky-nav-bar
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
--- site/profiles/admin.py
+++ site/profiles/admin.py
@@ -3,3 +3,7 @@ from .models import Profile
 
 
 admin.site.register(Profile)
+
+# resolve interface bug sticky-nav-bar
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
