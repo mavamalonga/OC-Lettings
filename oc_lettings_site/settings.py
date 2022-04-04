29c588330c0113bd0bc78a3e0968600935a62a99
Alpha <mavamalonga.alpha@gmail.com>
2022-04-04 21:52:51 +0200

    Allowed Hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://oc-lettings-treize.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['oc-lettings-treize.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
